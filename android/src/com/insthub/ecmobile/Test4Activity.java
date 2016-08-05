855bdd216840c527d7dd57676669c34e8e521bc3
suiyuchen <suiyc@xinboedu.com>
2016-08-05 17:21:51 +0800

    2016-08-05  测试创建新文件

---
+++ android/src/com/insthub/ecmobile/Test4Activity.java
@@ -0,0 +1,13 @@
+package com.insthub.ecmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test4Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test4);
+	}
+}
